105e60a56b66daec8d7af48e4718243ad0f4202b
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2026-06-13 19:59:08 +0530

    Add privateKey property to HostDevice class

--- shared/src/commonMain/kotlin/com/srilakshmikanthanp/clipbird/common/HostDevice.kt
+++ shared/src/commonMain/kotlin/com/srilakshmikanthanp/clipbird/common/HostDevice.kt
@@ -1,9 +1,11 @@
 package com.srilakshmikanthanp.clipbird.common
 
+import java.security.PrivateKey
 import java.security.PublicKey
 
 class HostDevice (
   override val id: Long,
   override val name: String,
-  override val publicKey: PublicKey
+  override val publicKey: PublicKey,
+  val privateKey: PrivateKey,
 ) : Device

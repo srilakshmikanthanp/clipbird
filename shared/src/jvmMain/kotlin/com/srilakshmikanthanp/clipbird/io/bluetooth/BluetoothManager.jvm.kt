736e95013c52e6596a4c9f29516d68ccada89b87
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2026-07-03 09:18:30 +0530

    Implement BluetoothManager with FFI integration for connection and server start

--- shared/src/jvmMain/kotlin/com/srilakshmikanthanp/clipbird/io/bluetooth/BluetoothManager.jvm.kt
+++ shared/src/jvmMain/kotlin/com/srilakshmikanthanp/clipbird/io/bluetooth/BluetoothManager.jvm.kt
@@ -4,18 +4,25 @@ import com.srilakshmikanthanp.clipbird.io.Channel
 import com.srilakshmikanthanp.clipbird.io.ClientFactory
 import com.srilakshmikanthanp.clipbird.io.Server
 import com.srilakshmikanthanp.clipbird.io.ServerFactory
+import com.srilakshmikanthanp.clipbird.io.bluetooth.ffi.BluetoothChannelFfi
+import com.srilakshmikanthanp.clipbird.io.bluetooth.ffi.BluetoothManagerFfi
+import com.srilakshmikanthanp.clipbird.io.bluetooth.ffi.BluetoothServerFfi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.withContext
 
-actual class BluetoothManager :
-  ClientFactory<BluetoothServerEndpoint>,
-  ServerFactory<BluetoothServerConfig> {
-  actual val boundedDevices: StateFlow<List<BluetoothDevice>> get() = TODO("Not yet implemented")
+actual class BluetoothManager : ClientFactory<BluetoothServerEndpoint>, ServerFactory<BluetoothServerConfig> {
+  private val bluetoothManagerFfi = BluetoothManagerFfi()
 
-  override suspend fun connect(endpoint: BluetoothServerEndpoint): Channel {
-    TODO("Not yet implemented")
+  actual val boundedDevices: StateFlow<List<BluetoothDevice>> = MutableStateFlow<List<BluetoothDevice>>(emptyList()).asStateFlow()
+
+  override suspend fun connect(endpoint: BluetoothServerEndpoint): Channel = withContext(Dispatchers.IO) {
+    BluetoothChannelFfi(bluetoothManagerFfi.connect(endpoint))
   }
 
-  override suspend fun start(config: BluetoothServerConfig): Server {
-    TODO("Not yet implemented")
+  override suspend fun start(config: BluetoothServerConfig): Server = withContext(Dispatchers.IO) {
+    BluetoothServerFfi(bluetoothManagerFfi.start(config))
   }
 }

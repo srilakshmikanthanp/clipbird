34d339886373fd6a7583c6cf7a88efc83d680128
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2026-07-04 13:23:22 +0530

    Add header file for clipbird with necessary includes

---
+++ shared/src/jvmMain/cpp/include/clipbird.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "error/error.h"
+#include "advertiser/advertiser.h"
+#include "advertiser/ble/ble_advertiser_factory.h"
+#include "io/channel.h"
+#include "io/server.h"
+#include "io/bluetooth/bluetooth_manager_factory.h"
+#include "io/bluetooth/bluetooth_manager.h"
+#include "log/log.h"

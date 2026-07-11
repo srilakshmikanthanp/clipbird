27f40719e1a84da658dae5039553f724215bc8b9
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2026-07-11 17:02:48 +0530

    Change AuthenticationException to extend Exception instead of PeerException

--- shared/src/commonMain/kotlin/com/srilakshmikanthanp/clipbird/authentication/AuthenticationException.kt
+++ shared/src/commonMain/kotlin/com/srilakshmikanthanp/clipbird/authentication/AuthenticationException.kt
@@ -6,4 +6,4 @@ import com.srilakshmikanthanp.clipbird.peer.PeerException
 class AuthenticationException(
   message: String,
   cause: Throwable? = null
-) : PeerException(ErrorCode.AUTHENTICATION_FAILED, message, cause)
+) : Exception(message, cause)

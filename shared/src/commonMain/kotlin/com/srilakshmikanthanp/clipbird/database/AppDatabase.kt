5fdab8e4cbd63744c36422f2dd2d85f3d3d2a86c
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2026-07-12 10:45:20 +0530

    Add Entity and DAO to AppDatabase

--- shared/src/commonMain/kotlin/com/srilakshmikanthanp/clipbird/database/AppDatabase.kt
+++ shared/src/commonMain/kotlin/com/srilakshmikanthanp/clipbird/database/AppDatabase.kt
@@ -3,10 +3,17 @@ package com.srilakshmikanthanp.clipbird.database
 import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.srilakshmikanthanp.clipbird.common.HostDeviceDao
+import com.srilakshmikanthanp.clipbird.common.HostDeviceEntity
 import com.srilakshmikanthanp.clipbird.paring.PairedDeviceEntity
+import com.srilakshmikanthanp.clipbird.paring.PairedDeviceEntityDao
+import com.srilakshmikanthanp.clipbird.paring.bluetooth.BluetoothPairedDeviceDao
 import com.srilakshmikanthanp.clipbird.paring.bluetooth.BluetoothPairedDeviceEntity
 
-@Database(entities = [PairedDeviceEntity::class, BluetoothPairedDeviceEntity::class], version = 1)
+@Database(entities = [PairedDeviceEntity::class, BluetoothPairedDeviceEntity::class, HostDeviceEntity::class], version = 1)
 @ConstructedBy(AppDatabaseConstructor::class)
 abstract class AppDatabase : RoomDatabase() {
+  abstract fun hostDeviceDao(): HostDeviceDao
+  abstract fun pairedDeviceDao(): PairedDeviceEntityDao
+  abstract fun bluetoothPairedDeviceDao(): BluetoothPairedDeviceDao
 }

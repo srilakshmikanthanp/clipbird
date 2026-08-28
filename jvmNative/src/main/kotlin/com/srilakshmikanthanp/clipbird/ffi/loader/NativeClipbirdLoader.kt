7651a878870f1cf08c40f515f6acb57891fb5c29
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2026-08-28 15:14:10 +0530

    Create temporary files in user home directory for native library loading

--- jvmNative/src/main/kotlin/com/srilakshmikanthanp/clipbird/ffi/loader/NativeClipbirdLoader.kt
+++ jvmNative/src/main/kotlin/com/srilakshmikanthanp/clipbird/ffi/loader/NativeClipbirdLoader.kt
@@ -1,6 +1,7 @@
 package com.srilakshmikanthanp.clipbird.ffi.loader
 
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.StandardCopyOption
 
 @Suppress("UnsafeDynamicallyLoadedCode")
@@ -13,8 +14,12 @@ object NativeClipbirdLoader {
       else -> error("Unsupported OS: ${System.getProperty("os.name")}")
     }
 
+    val dir = Path.of(System.getProperty("user.home"), ".clipbird")
+
+    Files.createDirectories(dir)
+
     val path = javaClass.getResourceAsStream("/native/libclipbird$extension")?.use { input ->
-      val temp = Files.createTempFile("clipbird-", extension)
+      val temp = Files.createTempFile(dir, "clipbird-", extension)
       temp.toFile().deleteOnExit()
       Files.copy(input, temp, StandardCopyOption.REPLACE_EXISTING)
       temp

787f59c85f73a56a75e4d025dd60e026adec2984
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2026-09-12 14:00:02 +0530

    Add link to snapshot release in downloads section

--- docs/web/src/sections/downloads/Download.tsx
+++ docs/web/src/sections/downloads/Download.tsx
@@ -1,5 +1,4 @@
-import { BsAndroid2 } from 'react-icons/bs';
-import { FaLinux } from 'react-icons/fa';
+import { FaGithub, FaLinux } from 'react-icons/fa';
 import Button from '../../components/button/Button';
 import classes from './download.module.css';
 
@@ -7,19 +6,19 @@ export default function Download() {
     return (
         <section className={classes.download} id="downloads">
             <h2>Downloads</h2>
-            <p>Get the latest release for your platform on GitHub.</p>
+            <p>Get the latest release or the rolling snapshot build on GitHub.</p>
             <div className={classes.links}>
                 <Button
                     icon={<FaLinux size={20} />}
                     href="https://github.com/srilakshmikanthanp/clipbird/releases/latest"
                     target="_blank">
-                    Linux
+                    Latest
                 </Button>
                 <Button
-                    icon={<BsAndroid2 size={20} />}
-                    href="https://github.com/srilakshmikanthanp/clipbird/releases/latest"
+                    icon={<FaGithub size={20} />}
+                    href="https://github.com/srilakshmikanthanp/clipbird/releases/tag/snapshot"
                     target="_blank">
-                    Android
+                    Snapshot
                 </Button>
             </div>
         </section>

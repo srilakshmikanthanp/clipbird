34b1e17d7a50df352985e8e8e17b34184f47e5c7
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2026-09-21 15:50:00 +0530

    Revert "Invoke stopDiscovery before StartDiscovery to clear any stale state"
    This reverts commit 81148e8401c844cfa1b637b0d476407a891b74f2.

--- jvmNative/src/main/cpp/src/discoverer/ble/linux/LinuxBleDiscoverer.cpp
+++ jvmNative/src/main/cpp/src/discoverer/ble/linux/LinuxBleDiscoverer.cpp
@@ -141,10 +141,6 @@ void LinuxBleDiscoverer::startDiscovery() {
       onAdapterPropertiesChanged(ifaceName, changedProps, invalidated);
     });
 
-  try {
-    adapterProxy->callMethodAsync("StopDiscovery").onInterface(kAdapterInterface).getResultAsFuture().get();
-  } catch (...) {}
-
   std::map<std::string, sdbus::Variant> filter;
 
   filter["DuplicateData"] = sdbus::Variant(true);
